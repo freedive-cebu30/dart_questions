f08e7f627604fee9656880b2b9b122a0affd067d
Joji <freedive.cebu30@gmail.com>
2022-03-25 22:57:08 +0800

    Create question_1.dart

---
+++ question_1.dart
@@ -0,0 +1,16 @@
+import 'dart:math' as math;
+
+void main() {
+  int number_1 = 10;
+  int number_2 = 3;
+  int number_3 = 2;
+  int number_4 = 10000;
+  
+  print("$number_1 + $number_2 = ${number_1 + number_2}");
+  print("$number_1 - $number_2 = ${number_1 - number_2}");
+  print("$number_1 * $number_2 = ${number_1 * number_2}");
+  print("$number_4 / $number_1 = ${number_4 / number_1}");
+  print("$number_1 / $number_2 = ${number_1 / number_2} 余り ${number_1 % number_2}");
+  print("$number_3の2乗 = ${math.pow(number_3, 2)}");
+  print("$number_3の3乗 = ${math.pow(number_3, 3)}");
+}
