473a57a640127dcb38e9fd25888e18b238c00a4d
Joji <freedive.cebu30@gmail.com>
2022-03-26 22:43:58 +0800

    Create question_4.dart

---
+++ question_4.dart
@@ -0,0 +1,17 @@
+void main() {
+  modulus(5);
+  modulus(7);
+  modulus(26);
+}
+
+void modulus(int num){
+   if (num > 25) {
+     print(num);
+   } else {
+     for (int $i = 1; $i <= 30; $i++) {
+       if ($i % num == 0) {
+         print($i);      
+       }
+     }
+   }
+}
