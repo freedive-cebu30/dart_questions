1903efa3688e754375aee5b2f55c8d9e35d5d81b
Joji <freedive.cebu30@gmail.com>
2022-03-29 16:49:41 +0800

    Create question_14.dart

---
+++ question_14.dart
@@ -0,0 +1,24 @@
+void main() {
+  String s1 = 'rubyab';
+  String s2 = 'railsb';
+  
+  var answer = compareString(s1, s2);
+  print(answer);
+  
+}
+
+String compareString(String str1, String str2){
+  List<String> subjectChars1 = str1.split('');
+  List<String> subjectChars2 = str2.split('');
+  List<String> repetitionChars = [];
+  
+  for (var str in subjectChars1) {
+    if(subjectChars2.contains(str)) {
+      if (!repetitionChars.contains(str)) {
+        repetitionChars.add(str);  
+      }
+    }
+  }
+  
+  return repetitionChars.join();
+}
