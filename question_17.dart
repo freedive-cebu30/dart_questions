52e3155ce15c05c3166f2942b2896af18b2cc31e
Joji <freedive.cebu30@gmail.com>
2022-03-29 22:53:36 +0800

    Update question_17.dart

--- question_17.dart
+++ question_17.dart
@@ -2,10 +2,14 @@ void main() {
   List<int> list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   int total = 0;
   
+  // forを使った方法
+  // the way of for
   for (int num in list1) {
     total += num;
   }
   print(total);
+  // reduceを使った方法
+  // the way of reduce
   total = list1.reduce((init, val) => init + val);
   print(total);
 }
