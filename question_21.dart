f87d4c9100a65b8d0df1ccd76722cbf7c468019b
Joji <freedive.cebu30@gmail.com>
2022-03-30 15:14:46 +0800

    Create question_21.dart

---
+++ question_21.dart
@@ -0,0 +1,20 @@
+void main() {
+  List<String> languages = ["ruby", "php", "python", "java"];
+  List<String> capLanguages = capitalize(languages);
+  
+  print(capLanguages);
+}
+
+List<String> capitalize(List<String> values){
+  List<String> capitalValues = [];
+  
+  for (String value in values) {
+    StringBuffer capBuffer = StringBuffer();
+    capBuffer..write(value[0].toUpperCase())
+             ..write(value.substring(1));  
+    
+    capitalValues.add(capBuffer.toString());
+  }
+
+  return capitalValues;
+}
