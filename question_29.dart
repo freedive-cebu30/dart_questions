53c1012ede7ddfe04bc50c86dcfcb30e09ba8011
Joji <freedive.cebu30@gmail.com>
2022-03-31 21:48:15 +0800

    Update question_29.dart

--- question_29.dart
+++ question_29.dart
@@ -2,6 +2,9 @@ main() {
   List values = [1, "a", 2, "b", "c", 3];
   List<int> intList = [];
   List<String> stringList = [];
+  
+  // どちらのやり方でも大丈夫です
+  // either way is fine
 
   for (var value in values) {
     if (value is int) {
@@ -10,9 +13,14 @@ main() {
       stringList.add(value);
     }
   }
-  
   print(intList);
   // [1, 2, 3]
   print(stringList);
   // [a, b, c]
+  
+  
+  // whereTypeを使うとシンプルに書けます
+  // It is easy to write using whereType
+  List<int> intList2 = values.whereType<int>().toList();
+  print(intList2);
 }
