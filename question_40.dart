0eefa98d8c98bcff42b56e8591c1dd44659d2556
Joji <freedive.cebu30@gmail.com>
2022-04-03 23:12:08 +0800

    Update question_40.dart

--- question_40.dart
+++ question_40.dart
@@ -1,8 +1,8 @@
 main() {
   var people = [{ "name" : "Taro1", "age" : 20, "height" : 180, "weight" : 60, "country" : "japan" },
                 { "name" : "Taro2", "age" : 30, "height" : 170, "weight" : 65 },
-                { "name" : "Taro3", "age" : 40, "height" : 160, "weight" : 70, "country" : 'Taiwan' },
-                { "name" : "Taro4", "age" : 50, "height" : 150, "weight" : 75, "country" : 'Japan' }];
+                { "name" : "Taro3", "age" : 40, "height" : 160, "weight" : 70, "country" : "Taiwan" },
+                { "name" : "Taro4", "age" : 50, "height" : 150, "weight" : 75, "country" : "Japan" }];
   
   var listPeople = [];
   
