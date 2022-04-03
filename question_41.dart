4e8ff71db0b4bc94e90bafe821cf909479ea6a16
Joji <freedive.cebu30@gmail.com>
2022-04-03 23:10:57 +0800

    Create question_41.dart

---
+++ question_41.dart
@@ -0,0 +1,16 @@
+main() {
+  var people = [{ "name" : "Taro1", "age" : 20, "height" : 180, "weight" : 60, "country" : "japan" },
+                { "name" : "Taro2", "age" : 30, "height" : 170, "weight" : 65 },
+                { "name" : "Taro3", "age" : 40, "height" : 160, "weight" : 70, "country" : "Taiwan" },
+                { "name" : "Taro4", "age" : 50, "height" : 150, "weight" : 75, "country" : "Japan" }];
+  
+  var listPeople = [];
+  
+  for (var person in people) {
+    if (person.containsValue("Japan")){
+      listPeople.add(person);
+    }
+  }
+
+  print(listPeople);
+}
