946b9e682556d89741603a1a83fa8f03eb3d3ec7
Joji <freedive.cebu30@gmail.com>
2022-04-07 17:27:37 +0800

    Update question_25.dart

--- question_25.dart
+++ question_25.dart
@@ -1,5 +1,10 @@
 void main() {
   List<int> values =  [1, 2, 3, 4, 5];
+  // どちらの書き方でもOKです
+  // Either way is fine
   List<int> doubleValues = values.map((value) => value * 2).toList();
   print(doubleValues);
+  
+  List<int> doubleValues2 = [for (var i in values) i * 2];
+  print(doubleValues2);
 }
