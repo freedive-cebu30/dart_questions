1e48690e146a8407c111a8220cd74a3118ce7d38
Joji <freedive.cebu30@gmail.com>
2022-04-07 17:25:30 +0800

    Update question_19.dart

--- question_19.dart
+++ question_19.dart
@@ -1,7 +1,12 @@
 void main() {
   List<String> languages = ["ruby", "php", "python", "java"];
+  // どちらの書き方でもOKです
+  // Either way is fine
   List<String> languages2 = languages.map((language) => language.toUpperCase())
                                      .toList()
                                      .cast<String>();
   print(languages2);
+  
+  List<String> languages3 = [for (var language in languages) language.toUpperCase()];
+  print(languages3);
 }
