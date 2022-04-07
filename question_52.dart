8806267ab571586b788edf848f8afc72b105a5dd
Joji <freedive.cebu30@gmail.com>
2022-04-07 11:04:49 +0800

    Update question_52.dart

--- question_52.dart
+++ question_52.dart
@@ -1,15 +1,16 @@
 import 'dart:math' as math;
 
 main() {
-  // 大文字のアルファベットの文字列を作成
+  // ASCIIコードのスタート
+  // Start of ASCII
   int bigLetterStart = 65;
   int bigLetterCount = 26;
 
   var alphabetArray = [];
   // 10個のアルファベットがある文字列を作成して、最後にjoinで繋げています
+  // create a 10 Strings and join it.
   var rand = math.Random();
   for (var i = 0; i < 10; i++) {
-    // 0-25の乱数を発生させます
     int number = rand.nextInt(bigLetterCount);
     int randomNumber = number + bigLetterStart;
     alphabetArray.add(String.fromCharCode(randomNumber));
