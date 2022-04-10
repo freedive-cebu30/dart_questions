2dbfbe10f645517fa459ac09d323fa9b5fa7e2d6
Joji <freedive.cebu30@gmail.com>
2022-04-10 17:45:33 +0800

    Update question_26.dart

--- question_26.dart
+++ question_26.dart
@@ -1,11 +1,11 @@
 void main() {
-    List<int> values = [1, 2, 3, 4, 5, 8, 9, 10, 18, 20];
-    List<int> values2 = [1, 2, 3, 4, 5, 8, 10, 20];
+  List<int> values = [1, 2, 3, 4, 5, 8, 9, 10, 18, 20];
+  List<int> values2 = [1, 2, 3, 4, 5, 8, 10, 20];
   
-   int answer1 = multiple9(values);
-   int answer2 = multiple9(values2);
-   print(answer1);
-   print(answer2);
+  int answer1 = multiple9(values);
+  int answer2 = multiple9(values2);
+  print(answer1);
+  print(answer2);
 }
 
 int multiple9 (List<int> values){
