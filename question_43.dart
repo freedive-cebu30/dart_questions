045ec1a88ac9eef53927929cc520b94964c99968
Joji <freedive.cebu30@gmail.com>
2022-04-12 12:11:31 +0800

    Update question_43.dart

--- question_43.dart
+++ question_43.dart
@@ -1,5 +1,5 @@
 main() {
-  List<Map<String, dynamic>> people = [{ "name" : "Taro1", "age" : 20, "height" : 180, "weight" : 60, "country" : "japan" },
+  List<Map<String, dynamic>> people = [{ "name" : "Taro1", "age" : 20, "height" : 180, "weight" : 60, "country" : "Japan" },
                                        { "name" : "Taro2", "age" : 30, "height" : 170, "weight" : 65 },
                                        { "name" : "Taro3", "age" : 40, "height" : 160, "weight" : 70, "country" : "Taiwan" },
                                        { "name" : "Taro4", "age" : 20, "height" : 160, "weight" : 75, "country" : "Japan" },
